966b265d05e16853019d83791cfa46f46cf2f0cf
Пярьков Егор <pyarkov2008@mail.ru>
2024-12-17 03:28:35 +0300

    Почти также работает

--- pkg/calculation/calculation.go
+++ pkg/calculation/calculation.go
@@ -5,140 +5,97 @@ import (
 	"strings"
 )
 
-func stringToFloat64(str string) float64 {
-	degree := float64(1)
-	var res float64 = 0
-	var invers bool = false
-	for i := len(str); i > 0; i-- {
-		if str[i-1] == '-' {
-			invers = true
-		} else {
-			res += float64(9-int('9'-str[i-1])) * degree
-			degree *= 10
-		}
-	}
-	if invers {
-		res = 0 - res
-	}
-	return res
-}
-
-func isSign(value rune) bool {
-	return value == '+' || value == '-' || value == '*' || value == '/'
-}
-
 func Calc(expression string) (float64, error) {
-	if len(expression) < 3 {
+	expression = strings.ReplaceAll(expression, " ", "")
+	if expression == "" {
 		return 0, ErrEmptyExpression
 	}
-	//////////////////////////////////////////////////////////////////////////////////////////////////////
-	var res float64
-	var b string
-	var c rune = 0
-	var resflag bool = false
-	var isc int
-	var countc int = 0
-	//////////////////////////////////////////////////////////////////////////////////////////////////////
-	for _, value := range expression {
-		if isSign(value) {
-			countc++
-		}
+
+	var nums []float64
+	var operations []rune
+
+	precedence := map[rune]int{
+		'+': 1,
+		'-': 1,
+		'*': 2,
+		'/': 2,
 	}
-	//////////////////////////////////////////////////////////////////////////////////////////////////////
-	if isSign(rune(expression[0])) || isSign(rune(expression[len(expression)-1])) {
-		return 0, ErrInvalidExpression
-	}
-	for i, value := range expression {
-		if value == '(' {
-			isc = i
+
+	applyOp := func() error {
+		if len(nums) < 2 || len(operations) == 0 {
+			return ErrInvalidExpression
 		}
-		if value == ')' {
-			calc, err := Calc(expression[isc+1 : i])
+		b := nums[len(nums)-1]
+		a := nums[len(nums)-2]
+		op := operations[len(operations)-1]
+		nums = nums[:len(nums)-2]
+		operations = operations[:len(operations)-1]
+
+		var result float64
+		switch op {
+		case '+':
+			result = a + b
+		case '-':
+			result = a - b
+		case '*':
+			result = a * b
+		case '/':
+			if b == 0 {
+				return ErrDivisionByZero
+			}
+			result = a / b
+		default:
+			return ErrInvalidExpression
+		}
+		nums = append(nums, result)
+		return nil
+	}
+
+	i := 0
+	for i < len(expression) {
+		if expression[i] >= '0' && expression[i] <= '9' {
+			start := i
+			for i < len(expression) && (expression[i] >= '0' && expression[i] <= '9' || expression[i] == '.') {
+				i++
+			}
+			num, err := strconv.ParseFloat(expression[start:i], 64)
 			if err != nil {
+				return 0, err
+			}
+			nums = append(nums, num)
+			continue
+		}
+
+		if expression[i] == '(' {
+			operations = append(operations, '(')
+		} else if expression[i] == ')' {
+			for len(operations) > 0 && operations[len(operations)-1] != '(' {
+				if err := applyOp(); err != nil {
+					return 0, err
+				}
+			}
+			if len(operations) == 0 {
 				return 0, ErrInvalidExpressionInParentheses
 			}
-			calcstr := strconv.FormatFloat(calc, 'f', 0, 64)
-			i2 := i
-			i -= len(expression[isc:i+1]) - len(calcstr)
-			expression = strings.Replace(expression, expression[isc:i2+1], calcstr, 1) // Меняем скобки на результат выражения в них
+			operations = operations[:len(operations)-1]
+		} else {
+			for len(operations) > 0 && precedence[rune(expression[i])] <= precedence[operations[len(operations)-1]] {
+				if err := applyOp(); err != nil {
+					return 0, err
+				}
+			}
+			operations = append(operations, rune(expression[i]))
+		}
+		i++
+	}
+	for len(operations) > 0 {
+		if err := applyOp(); err != nil {
+			return 0, err
 		}
 	}
-	if countc > 1 {
+	if len(nums) != 1 {
+		return 0, ErrInvalidExpression
+	}
 
-		for i := 1; i < len(expression); i++ {
-			value := rune(expression[i])
-			///////////////////////////////////////////////////////////////////////////////////////////////////////////////
-			//Умножение и деление
-			if value == '*' || value == '/' {
-				if rune(expression[i+1]) == '0' && value == '/' {
-					return 0, ErrDivisionByZero
-				}
-				var imin int = i - 1
-				if imin != 0 {
-					for !isSign(rune(expression[imin])) && imin > 0 {
-						imin--
-					}
-					imin++
-				}
-				var imax int = i + 1
-				if imax == len(expression) {
-					imax--
-				} else {
-					for !isSign(rune(expression[imax])) && imax < len(expression)-1 {
-						imax++
-					}
-				}
-				if imax == len(expression)-1 {
-					imax++
-				}
-				calc, err := Calc(expression[imin:imax])
-				if err != nil {
-					if value == '*' {
-						return 0, ErrMultiplyError
-					} else {
-						return 0, ErrDivisionError
-					}
-				}
-				calcstr := strconv.FormatFloat(calc, 'f', 0, 64)
-				i -= len(expression[isc:i+1]) - len(calcstr) - 1
-				expression = strings.Replace(expression, expression[imin:imax], calcstr, 1) // Меняем умнжение/деление на результат выражения
-			}
-			if value == '+' || value == '-' || value == '*' || value == '/' {
-				c = value
-			}
-		}
-	}
-	//////////////////////////////////////////////////////////////////////////////////////////////////////
-	for _, value := range expression + "s" {
-		switch {
-		case value == ' ':
-			continue
-		case value > 47 && value < 58: // Если это цифра
-			b += string(value)
-		case isSign(value) || value == 's': // Если это знак
-			if resflag {
-				switch c {
-				case '+':
-					res += stringToFloat64(b)
-				case '-':
-					res -= stringToFloat64(b)
-				case '*':
-					res *= stringToFloat64(b)
-				case '/':
-					res /= stringToFloat64(b)
-				}
-			} else {
-				resflag = true
-				res = stringToFloat64(b)
-			}
-			b = strings.ReplaceAll(b, b, "")
-			c = value
-
-			/////////////////////////////////////////////////////////////////////////////////////////////
-		case value == 's':
-		default:
-			return 0, ErrInvalidExpression
-		}
-	}
-	return res, nil
+	return nums[0], nil
 }
